6d192400fbe507653dd91a4a44541a62215d2a17
evie33 <evieren90@gmail.com>
2018-07-05 12:30:23 -0400

    1st

--- game/index.js
+++ game/index.js
@@ -1,6 +1,24 @@
 import {Map} from 'immutable'
+import { createStore } from 'redux';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const MOVE = 'MOVE'
+
+export const move =  (player, position)=>{
+  return {
+    type: MOVE,
+    position: position,
+    player: player
+  };
 }
+
+const initialState= {position:[], player: 'X'};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type){
+    case MOVE:
+    return {position: action.position, player: action.player}
+    default:
+    return state
+  }
+}
+
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const printBoard = () => {
 }
 
 const getInput = player => async () => {
-  const {turn} = game.getState()  
+  const {turn} = game.getState()
   if (turn !== player) return
   const ans = await inquirer.prompt([{
     type: 'input',
